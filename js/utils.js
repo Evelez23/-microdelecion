0b8da38e4efccdacae5207e2a79c8ef0f528a719
Evelez23 <elmer_velez@hotmail.com>
2025-09-06 18:07:19 -0600

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,17 +1,17 @@
 // utils.js - Funciones de utilidad general
 
-function getProxyUrl(url) {
+window.getProxyUrl = function(url) {
     return `https://corsproxy.io/?${encodeURIComponent(url)}`;
-}
+};
 
-function checkCollision(obj1, obj2) {
+window.checkCollision = function(obj1, obj2) {
     return obj1.x < obj2.x + obj2.width &&
            obj1.x + obj1.width > obj2.x &&
            obj1.y < obj2.y + obj2.height &&
            obj1.y + obj1.height > obj2.y;
-}
+};
 
-function createParticles(x, y, count, color) {
+window.createParticles = function(x, y, count, color) {
     for (let i = 0; i < count; i++) {
         particles.push({
             x: x,
@@ -23,9 +23,9 @@ function createParticles(x, y, count, color) {
             life: 30
         });
     }
-}
+};
 
-function createTextParticle(x, y, text, color) {
+window.createTextParticle = function(x, y, text, color) {
     particles.push({
         x: x,
         y: y,
@@ -34,12 +34,12 @@ function createTextParticle(x, y, text, color) {
         life: 60,
         type: 'text'
     });
-}
+};
 
-function drawCloud(x, y) {
+window.drawCloud = function(x, y) {
     ctx.beginPath();
     ctx.arc(x, y, 20, 0, Math.PI * 2);
     ctx.arc(x + 15, y - 10, 15, 0, Math.PI * 2);
     ctx.arc(x + 35, y, 25, 0, Math.PI * 2);
     ctx.fill();
-}
+};
